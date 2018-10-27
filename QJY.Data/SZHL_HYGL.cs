db8c8d0b36277c974c6be9b38fa8c51cb7de4c17
dev-xiangyi <279113574@qq.com>
2018-10-27 15:20:12 +0800

    会议视频修改

--- QJY.Data/SZHL_HYGL.cs
+++ QJY.Data/SZHL_HYGL.cs
@@ -69,5 +69,11 @@ namespace QJY.Data
         public string LocationImg { get; set; }
         public Nullable<int> IsOff { get; set; }
         public string Template { get; set; }
+        public string Video1Title { get; set; }
+        public string Video1Url { get; set; }
+        public string Video2Title { get; set; }
+        public string Video2Url { get; set; }
+        public string Video3Title { get; set; }
+        public string Video3Url { get; set; }
     }
 }
